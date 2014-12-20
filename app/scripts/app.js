2a5197f85ab57bb2bd0dbd54a136d8ea47180573
lichunqiang <light-li@hotmail.com>
2014-12-20 08:53:01 +0800

    Initialize

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lxpgwApp
+ * @description
+ * # lxpgwApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lxpgwApp', [
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lxpgwApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lxpgwApp
+ */
+angular.module('lxpgwApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
